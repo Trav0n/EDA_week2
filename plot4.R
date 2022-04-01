84946468b3efde3003d7191c6505149739a3dfd5
Trav0n <souzatravon17@gmail.com>
2022-04-01 20:09:36 +0530

    first commit

---
+++ plot4.R
@@ -0,0 +1,7 @@
+setwd("c:/Users/user/eda")
+coalMatches  <- grepl("coal", SCC$Short.Name, ignore.case=TRUE)
+subsetSCC <- SCC[coalMatches, ]
+NEISCC <- merge(NEI, subsetSCC, by="SCC")
+totalEmissions <- tapply(NEISCC$Emissions, NEISCC$year, sum)
+barplot(totalEmissions, xlab = "Year", ylab = "Total Emission (ton)", 
+        main = "Total Emission from coal sources")
